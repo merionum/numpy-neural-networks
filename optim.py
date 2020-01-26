8b8c3842b2590102e876e32f3388c0c3a5914ade
Vadim Gudkov <vadim0006@gmail.com>
2020-01-26 04:15:55 +0300

    Create optim.py

---
+++ optim.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class SGD:
+    """
+    Implements vanilla SGD update
+    """
+    def update(self, w, d_w, learning_rate):
+        """
+        Performs SGD update
+
+        Arguments:
+        w, np array - weights
+        d_w, np array, same shape as w - gradient
+        learning_rate, float - learning rate
+
+        Returns:
+        updated_weights, np array same shape as w
+        """
+        return w - d_w * learning_rate
+
+
+class MomentumSGD:
+    """
+    Implements Momentum SGD update
+    """
+    def __init__(self, momentum=0.9):
+        self.momentum = 0.9
+        self.new = True
+
+    def update(self, w, d_w, learning_rate):
+        """
+        Performs Momentum SGD update
+
+        Arguments:
+        w, np array - weights
+        d_w, np array, same shape as w - gradient
+        learning_rate, float - learning rate
+
+        Returns:
+        updated_weights, np array same shape as w
+        """
+        if self.new:
+            self.velocity = np.zeros_like(w)
+            self.new -= 1
+        
+        self.velocity = self.momentum * self.velocity - learning_rate * d_w 
+        return w + self.velocity
